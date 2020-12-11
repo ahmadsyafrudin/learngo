4f12afc98ea360c44f3f4d481257b95e4bc6d1c9
Rudi <rudi@Ahmads-MacBook-Pro-2.local>
2020-12-11 21:10:06 +0700

    added example struct

--- struct/main.go
+++ struct/main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
 func main() {
-	fmt.Println("Hello Go!")
+	john := person{
+		firstName: "John",
+		lastName:  "Doe",
+		contact: contactInfo{
+			email:   "john@me.local",
+			zipCode: 9400,
+		},
+	}
+
+	fmt.Printf("%+v", john)
 }
